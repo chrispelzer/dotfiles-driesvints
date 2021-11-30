0442af9c9542fd775a9e9e64954303d65c9afe55
Chris Pelzer <chrispelzer@gmail.com>
2021-11-30 05:23:39 -0500

    fix brew doctor warnings and alias with the coreutils

--- path.zsh
+++ path.zsh
@@ -1,3 +1,5 @@
+export PATH="/usr/local/sbin:$PATH"
+
 # Load dotfiles binaries
 export PATH="$DOTFILES/bin:$PATH"
 
@@ -10,12 +12,10 @@ export PATH="$HOME/.node/bin:$PATH"
 # Use project specific binaries before global ones
 export PATH="node_modules/.bin:vendor/bin:$PATH"
 
-export PATH="/usr/local/opt/coreutils/libexec/gnubin:$PATH"
-
 # Make sure coreutils are loaded before system commands
 # I've disabled this for now because I only use "ls" which is
 # referenced in my aliases.zsh file directly.
-#export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
+# export PATH="$(brew --prefix coreutils)/libexec/gnubin:$PATH"
 
 export NVM_DIR="$([ -z "${XDG_CONFIG_HOME-}" ] && printf %s "${HOME}/.nvm" || printf %s "${XDG_CONFIG_HOME}/nvm")"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
